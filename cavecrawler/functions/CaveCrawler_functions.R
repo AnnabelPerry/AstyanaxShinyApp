c6cade608c8eb9bbfbafa579c15938ca9a00b850
AnnabelPerry <87875766+AnnabelPerry@users.noreply.github.com>
2022-06-19 19:43:48 -0500

    prepping for next exercise

--- cavecrawler/functions/CaveCrawler_functions.R
+++ cavecrawler/functions/CaveCrawler_functions.R
@@ -26,7 +26,7 @@ world_map <- subset(world_map_1, region %in% c("USA", "Mexico", "Belize","Guatem
 
 
 # Read in gene position data
-position_table <- read.csv("data/PositionTable.csv", fill = TRUE)
+position_table <- read.csv("data/AmexPositionTable.csv", fill = TRUE)
 # Remove duplicates so multiple rows with the same values are not outputted
 position_table <- position_table[!duplicated(position_table$Gene_ID),]
 
--- data_preparation_scripts/position_table.R
+++ data_preparation_scripts/position_table.R
@@ -15,10 +15,15 @@ gtf <- read.table("Astyanax_mexicanus.Astyanax_mexicanus-2.0.106.gtf/Astyanax_me
 gtf <- gtf[,c(1,4:5,10,16)]
 colnames(gtf) <- c("Scaffold","Start_Locus","End_Locus","Gene_ID","Gene_Name")
 
+# Ensembl outputs all genes with no official gene name as "ensembl". Change this
+# to "NA"
+gtf$Gene_Name[grepl("ensembl", gtf$Gene_Name)] <- NA
+
 # Remove duplicate gene names as well as transcript IDs (the data in CaveCrawler 
 # currently does not have the precision to tell the user which specific 
 # transcript is yielding a given signal)
-gtf <- gtf[(!grepl("ENSAMXT", gtf$Gene_Name) & !duplicated(gtf$Gene_Name)), ]
+gtf <- gtf[!grepl("ENSAMXT", gtf$Gene_Name), ]
+gtf <- gtf[!duplicated(gtf$Gene_ID),]
 
 # Save file as a position table to be drawn upon in the "functions.R" file
 write.csv(gtf, "PositionTable.csv", row.names = F)
